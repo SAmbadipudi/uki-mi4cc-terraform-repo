1227edc326655fbbb7004f51a8b5a03cb21e16f9
SAmbadipudi <vmsrikanth.a@gmail.com>
2019-09-21 10:11:00 +0000

    kK,

--- layers/cloudwatch-sns/main.tf
+++ layers/cloudwatch-sns/main.tf
@@ -5,7 +5,7 @@ module "cloudwatch-sns" {
     "aws" = "aws.mi4cc"
   }
 
-  cloudwatch_event_rule_name_for_sns = "${var.source_market_name}-${lower(var.environment)}-cloudwatch-${var.project_name}-avaya-alerts"
+  cloudwatch_event_rule_name_for_sns = "${var.source_market_name}-${lower(var.environment, terraform.workspace)}-cloudwatch-${var.project_name}-avaya-alerts"
   cloudwatch_event_rule_description_for_sns = "${var.cloudwatch_event_rule_description_for_sns}"
   event_pattern = "${file("${path.module}/file.json")}"
   target-id = "${var.target-id}"
