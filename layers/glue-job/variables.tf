ccdd9434a318683fda9cf57cb95e5c5a6cf26aa2
SAmbadipudi <45809793+SAmbadipudi@users.noreply.github.com>
2019-09-22 16:40:18 +0530

    bhszzkc

--- layers/glue-job/variables.tf
+++ layers/glue-job/variables.tf
@@ -1,12 +1,12 @@
 variable "connections" {
   description = "AZ for the ASG to stick to"
-  type        = list(string)
+  type        = "list"
   default     = ["uki-con-mi4cc-avaya"]
 }
 
 variable "jobnames" {
   description = "glue job name"
-  type  = list(string)
+  type  = "list"
   default  = ["uki-mi4cc-dev-one","uki-mi4cc-dev-two","uki-mi4cc-dev-three"]
 }
 
@@ -24,4 +24,3 @@ variable "extra-jars" {
   description = "JDBC jar file location to establish connection with Informix DB"
   default     = "s3://uki-dev-sri-s3-mi4cc/uki/contactcentre/avaya/cache-jdbc/cache-jdbc-2.0.0.jar"
 }
-
