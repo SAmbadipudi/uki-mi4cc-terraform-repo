fd4565563a98414f9acf7194e955265851be90a5
SAmbadipudi <45809793+SAmbadipudi@users.noreply.github.com>
2019-09-23 17:41:21 +0530

    erytrtuert

--- layers/glue-job/providers.tf
+++ layers/glue-job/providers.tf
@@ -2,6 +2,6 @@ provider "aws" {
   alias  = "mi4cc"
   region = "${var.region}"
   assume_role {
-    role_arn = "arn:aws:iam::${lookup(var.mi4cc_account_id, terraform.workspace)}:role/uki_iam_role_jenkins_user"
+    role_arn = "arn:aws:iam::${lookup(var.mi4cc_account_id, terraform.workspace)}:role/test_Lambda"
   }
 }
